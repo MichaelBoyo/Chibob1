e021ebe55e2c45291467db02391ec2d401468fbe
MichaelBoyo <103725264+MichaelBoyo@users.noreply.github.com>
2022-04-25 23:44:50 +0100

    new

--- test/KataTDD/AreaOfTriangleTest.java
+++ test/KataTDD/AreaOfTriangleTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AreaOfTriangleTest {
     @Test
     public void areaOfTriangleTest(){
-        AreaOfTriangle isocelesTriangle = new AreaOfTriangle();
-        assertEquals(34, isocelesTriangle.sides(1.5,-3.4,4.6,5,9.5,-3.4));
+        AreaOfTriangle isoscelesTriangle = new AreaOfTriangle();
+        assertEquals(34.0, isoscelesTriangle.sides(1.5,-3.4,4.6,5,9.5,-3.4));
     }
 }
